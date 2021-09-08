a7782323858244f7e2e4cb27638589fec455d18a
przygoda <schmidt.multimedia@googlemail.com>
2021-09-08 10:21:14 +0000

    AUTO_INCREMENT ++

--- 02_SQL_Tables/02c_db_table_SQL.sql
+++ 02_SQL_Tables/02c_db_table_SQL.sql
@@ -1,7 +1,7 @@
 /* ------  Strukturen UNIQUE ----- */
 
 /* 	 
-	KEY
+	Primary key + Autoincrement
 */
 
 /* DB boo löschen, falls vorhanden*/
@@ -17,7 +17,7 @@ CREATE DATABASE IF NOT EXISTS boo;
 /* Tabelle anlegen, falls noch nicht vorhanden */
 CREATE TABLE IF NOT EXISTS boo.test
 (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(20) NOT NULL DEFAULT "TBA",
     age INT NOT NULL DEFAULT 0
 );
@@ -26,10 +26,11 @@ CREATE TABLE IF NOT EXISTS boo.test
 DESCRIBE boo.test;
 
 /* ----- Daten ------- 1a: zB. Index-Einlesen von Tabellen */
-INSERT INTO boo.test(id,name,age) VALUES (1,"Grizabella", 29);
-INSERT INTO boo.test(id,name,age) VALUES (2,"Alonzo", 35);
-INSERT INTO boo.test(id,name,age) VALUES (3,"Alonzo", 24);
-INSERT INTO boo.test(id,name,age) VALUES (4,"Maxine", 26);
+INSERT INTO boo.test(name,age) VALUES ("Grizabella", 29);
+INSERT INTO boo.test(name,age) VALUES ("Alonzo", 35);
+INSERT INTO boo.test(name,age) VALUES ("Alonzo", 24);
+INSERT INTO boo.test(name,age) VALUES ("Maxine", 26);
+INSERT INTO boo.test(name,age) VALUES ("Maxine", 26);
 
 /* Inhalte der Tabelle anzeigen */
 SELECT * FROM boo.test;
