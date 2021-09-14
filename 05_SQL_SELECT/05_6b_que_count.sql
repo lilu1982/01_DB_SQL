640d910c697a6b4692fa8a35ba6be9533945973e
przygoda <schmidt.multimedia@googlemail.com>
2021-09-14 08:41:35 +0000

    GROUP BY / HAVING

--- 05_SQL_SELECT/05_6b_que_count.sql
+++ 05_SQL_SELECT/05_6b_que_count.sql
@@ -7,7 +7,7 @@
 */
 
 
--- welche VERSCHIEDENEN Sektoren gibt es?
+-- Welche VERSCHIEDENEN Sektoren gibt es?
 /*
 SELECT
     DISTINCT sector Industriesektoren
@@ -15,3 +15,53 @@ FROM stocks.ccc
 ORDER BY sector ASC
 ;
 */
+
+-- Wieviele VERSCHIEDENE Sektoren ?
+/*
+SELECT
+    COUNT(DISTINCT sector) "Anzahl Sektoren"
+FROM stocks.ccc
+;
+*/
+
+-- Welche VERSCHIEDENEN Branchen ?
+/*
+SELECT
+    DISTINCT industry Branchen
+FROM stocks.ccc
+ORDER BY Branchen ASC
+;
+*/
+
+-- Wieviele VERSCHIEDENE Branchen ?
+/*
+SELECT
+    COUNT(DISTINCT industry) "Anzahl Branchen"
+FROM stocks.ccc
+;
+*/
+
+-- Wieviele VERSCHIEDENE Branchen gibt es 
+-- in den jeweiligen Industriesektoren?
+SELECT
+    sector Industriesektoren,  -- nicht aggregiert / organisch
+    COUNT(DISTINCT industry) Branchen -- aggregiert / durch eine Fkt. entstanden
+FROM stocks.ccc
+WHERE sector LIKE "Co%" -- Filter in NICHT aggr. Feldern 
+GROUP BY sector -- bei Kombination nicht aggr. /aggr.
+HAVING Branchen > 10 -- Filter in aggr. Felder / nach GROUP
+ORDER BY Branchen DESC
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
