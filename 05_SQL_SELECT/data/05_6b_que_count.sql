fd2cbcb7f5e6cece95bf0e65a0fcae66c3150e2e
lilu1982 <homenetmedia82@gmail.com>
2021-09-14 08:41:48 +0000

    GROUP BY /  HAVING

--- 05_SQL_SELECT/data/05_6b_que_count.sql
+++ 05_SQL_SELECT/data/05_6b_que_count.sql
@@ -11,12 +11,47 @@ LIMIT 50
 */
 
 /*
---nur das erste Auftreten von Sektoren
--- welche Verschiedenen Sektoren gibt es?
+--Welche verschiedenen Branchen ?
 SELECT
-    DISTINCT sector Industriesektoren
+    DISTINCT industry Branchen
 FROM stocks.ccc
-ORDER BY sector ASC
-# LIMIT 50
+ORDER BY Branchen ASC;
+*/
+
+/*
+--Wieviele VERSCHIEDENE Branchen?
+SELECT
+    COUNT(DISTINCT) "Anzahl Branchen)
+FROM stocks.ccc
 ;
 */
+
+
+
+-- Wieviele VERSCHIEDENE Brahnchen gibt es
+-- in den jewiligen Industriesektoren?
+
+SELECT 
+    sector Industriesektoren, -- nicht aggregiert / organisch
+    COUNT(DISTINCT industry) Branchen-- aggregiert / durch eine (Fkt.) Funktion erstanden
+FROM stocks.ccc
+WHERE sector Like "Co%"  -- Filter in nicht aggr. Feldern 
+Group BY sector -- bei Kombination nicht aggr. /aggr.
+HAVING Branchen > 10 -- Filter in aggr. Felder / nach Group
+ORDER BY Branchen DESC -- DESC sortieren 
+# LIMIT 5 -- auf (5) Daten begrenzen
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
